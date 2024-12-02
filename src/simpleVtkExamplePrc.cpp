b63e830524e762ee148b61da0beb29cb168da7f6
Vladislav Kazankov <teronos@qfteam.org>
2024-12-02 18:07:33 +0300

    small fix

--- src/simpleVtkExamplePrc.cpp
+++ src/simpleVtkExamplePrc.cpp
@@ -1,5 +1,6 @@
-#include "lib.h"
+﻿#include "lib.h" // нужно для подключения статических библиотек проекта
 
+// далее идет код hello world для VTK https://examples.vtk.org/site/Cxx/GeometricObjects/CylinderExample/
 #include <vtkActor.h>
 #include <vtkCamera.h>
 #include <vtkCylinderSource.h>
@@ -14,60 +15,60 @@
 #include <array>
 
 int main(int, char* [])
-{   
-    vtkCylinderSource* cylinder = vtkCylinderSource::New();
+{
+    vtkNew<vtkNamedColors> colors;
+
+    // Set the background color.
+    std::array<unsigned char, 4> bkg{ {26, 51, 102, 255} };
+    colors->SetColor("BkgColor", bkg.data());
+
+    // This creates a polygonal cylinder model with eight circumferential facets
+    // (i.e, in practice an octagonal prism).
+    vtkNew<vtkCylinderSource> cylinder;
     cylinder->SetResolution(8);
 
     // The mapper is responsible for pushing the geometry into the graphics
-    // library. It may also do color mapping, if scalars or other attributes
-    // are defined.
-    //
-    vtkPolyDataMapper* cylinderMapper = vtkPolyDataMapper::New();
+    // library. It may also do color mapping, if scalars or other attributes are
+    // defined.
+    vtkNew<vtkPolyDataMapper> cylinderMapper;
     cylinderMapper->SetInputConnection(cylinder->GetOutputPort());
 
     // The actor is a grouping mechanism: besides the geometry (mapper), it
     // also has a property, transformation matrix, and/or texture map.
-    // Here we set its color and rotate it -22.5 degrees.
-    vtkActor* cylinderActor = vtkActor::New();
+    // Here we set its color and rotate it around the X and Y axes.
+    vtkNew<vtkActor> cylinderActor;
     cylinderActor->SetMapper(cylinderMapper);
-    cylinderActor->GetProperty()->SetColor(1.0000, 0.3882, 0.2784);
+    cylinderActor->GetProperty()->SetColor(
+        colors->GetColor4d("Tomato").GetData());
     cylinderActor->RotateX(30.0);
     cylinderActor->RotateY(-45.0);
 
-    // Create the graphics structure. The renderer renders into the
-    // render window. The render window interactor captures mouse events
+    // The renderer generates the image
+    // which is then displayed on the render window.
+    // It can be thought of as a scene to which the actor is added
+    vtkNew<vtkRenderer> renderer;
+    renderer->AddActor(cylinderActor);
+    renderer->SetBackground(colors->GetColor3d("BkgColor").GetData());
+    // Zoom in a little by accessing the camera and invoking its "Zoom" method.
+    renderer->ResetCamera();
+    renderer->GetActiveCamera()->Zoom(1.5);
+
+    // The render window is the actual GUI window
+    // that appears on the computer screen
+    vtkNew<vtkRenderWindow> renderWindow;
+    renderWindow->SetSize(300, 300);
+    renderWindow->AddRenderer(renderer);
+    renderWindow->SetWindowName("Cylinder");
+
+    // The render window interactor captures mouse events
     // and will perform appropriate camera or actor manipulation
     // depending on the nature of the events.
-    //
-    vtkRenderer* ren1 = vtkRenderer::New();
-    vtkRenderWindow* renWin = vtkRenderWindow::New();
-    renWin->AddRenderer(ren1);
-    vtkRenderWindowInteractor* iren = vtkRenderWindowInteractor::New();
-    iren->SetRenderWindow(renWin);
-
-    // Add the actors to the renderer, set the background and size
-    //
-    ren1->AddActor(cylinderActor);
-    ren1->SetBackground(0.1, 0.2, 0.4);
-    renWin->SetSize(200, 200);
-
-    // We'll zoom in a little by accessing the camera and invoking a "Zoom"
-    // method on it.
-    ren1->ResetCamera();
-    ren1->GetActiveCamera()->Zoom(1.5);
-    renWin->Render();
+    vtkNew<vtkRenderWindowInteractor> renderWindowInteractor;
+    renderWindowInteractor->SetRenderWindow(renderWindow);
 
     // This starts the event loop and as a side effect causes an initial render.
-    iren->Start();
+    renderWindow->Render();
+    renderWindowInteractor->Start();
 
-    // Exiting from here, we have to delete all the instances that
-    // have been created.
-    cylinder->Delete();
-    cylinderMapper->Delete();
-    cylinderActor->Delete();
-    ren1->Delete();
-    renWin->Delete();
-    iren->Delete();
-
-    return 0;
+    return EXIT_SUCCESS;
 }
